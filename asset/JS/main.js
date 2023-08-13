a2040143eeebd7fe9f1f30747c7b7bb1ef11408b
Marzieh hoseini <marzyahhooseini74@gmail.com>
2023-08-13 15:09:41 +0330

    comments fixed and make recent newa section

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -32,52 +32,120 @@ _openMenu.addEventListener('click', ()=>{
 
 
 // owl
-$('.owl-carousel.slider_carousel').owlCarousel({
-    loop:true,
-    dots: true,
-    margin:30,
-    stagePadding:2,
-    autoplay: true,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        300:{
-            items: 1
-        },
-        600:{
-            items:1
-        },
-        800:{
-            items:2
-        },
-        1200:{
-            items:3
-        },
-        1400:{
-            items: 4
-        }
-    }
-})
-$('.comments').owlCarousel({
+// $('.owl-carousel.slider_carousel').owlCarousel({
+//     loop:true,
+//     dots: true,
+//     margin:30,
+//     stagePadding:2,
+//     autoplay: true,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         300:{
+//             items: 1
+//         },
+//         600:{
+//             items:1
+//         },
+//         800:{
+//             items:2
+//         },
+//         1200:{
+//             items:3
+//         },
+//         1400:{
+//             items: 4
+//         }
+//     }
+// })
 
-    responsive:{
-        0:{
-            items:1
-        },
-        300:{
-            items: 1
-        },
-        600:{
-            items:1
-        },
-        800:{
-            items:2
-        },
-        1200:{
-            items:3
-        }
-    }
-})
+// $(document).ready(function() {
+//     var one = $(".comments");
+
+
+// //    one.owlCarousel({
+// //         loop: true,
+// //         // dots: ($(".owl-carousel .item").length > 1) ? true : false,
+// //         autoplay: false,
+        
+// //         margin:0,
+// //         // navigation: true,
+// //         // stagePadding:,
+// //         // nav: false,
+// //         responsive:{
+// //             0:{
+// //                 items:1
+// //             },
+// //             300:{
+// //                 items: 1
+// //             },
+// //             576:{
+// //                 items:1
+// //             },
+// //             992:{
+// //                 items:2
+// //             },
+// //             1200:{
+// //                 items:4
+// //             }
+// //         }
+// //     })
+
+// })
+
+$(".trends").owlCarousel({
+    items: 4,
+    nav: true,
+    loop: true,
+    mouseDrag: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      767: {
+        items: 3,
+      },
+      992: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+$(".comment").owlCarousel({
+    items: 3,
+    nav: true,
+    loop: true,
+    // dots: true,
+    mouseDrag: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+      1400: {
+        items: 3,
+      },
+    },
+  });
+
 
